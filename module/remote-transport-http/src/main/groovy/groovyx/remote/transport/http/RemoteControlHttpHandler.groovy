596e555204e0f0474cd54bd529567c3325c85bfa
Luke Daley <ld@ldaley.com>
2010-11-01 17:56:33 +1000

    Better subclassing API.

--- module/remote-transport-http/src/main/groovy/groovyx/remote/transport/http/RemoteControlHttpHandler.groovy
+++ module/remote-transport-http/src/main/groovy/groovyx/remote/transport/http/RemoteControlHttpHandler.groovy
@@ -19,6 +19,9 @@ import com.sun.net.httpserver.HttpHandler
 import com.sun.net.httpserver.HttpExchange
 import groovyx.remote.server.Receiver
 
+/**
+ * A HttpHandler implementation for the com.sun.net.httpserver package.
+ */
 class RemoteControlHttpHandler implements HttpHandler {
 
 	final Receiver receiver
@@ -28,23 +31,56 @@ class RemoteControlHttpHandler implements HttpHandler {
 	}
 
 	void handle(HttpExchange exchange) {
+		if (validateRequest(exchange)) {
+			configureSuccessfulResponse(exchange)
+
+			exchange.responseBody.withStream {
+				doExecute(exchange.requestBody, it)
+			}
+		}
+	}
+
+	/**
+	 * Validate that this request is valid.
+	 * 
+	 * Subclasses should call this implementation before any custom validation.
+	 * 
+	 * If the request is invalid, this is the place to send back the appropriate headers/body.
+	 * 
+	 * @return true if the request is valid and should proceed, false if otherwise.
+	 */
+	protected boolean validateRequest(HttpExchange exchange) {
 		if (exchange.requestMethod != "POST") {
 			exchange.sendResponseHeaders(415, 0)
 			exchange.responseBody.withStream { it << "request must be a POST" }
-			return
+			return false
 		}
 		
 		if (exchange.requestHeaders.getFirst("Content-Type") != ContentType.COMMAND.value) {
 			exchange.sendResponseHeaders(415, 0)
 			exchange.responseBody.withStream { it << "Content type must be: ${ContentType.COMMAND.value}" }
-			return
+			return false
 		}
-
+		
+		true
+	}
+	
+	/**
+	 * Called when a request has been validated.
+	 * 
+	 * Subclasses should call this implementation to set the status code and return content type.
+	 */
+	protected void configureSuccessfulResponse(HttpExchange exchange) {
 		exchange.responseHeaders.set("Content-Type", ContentType.RESULT.value)
 		exchange.sendResponseHeaders(200, 0)
-		
-		exchange.responseBody.withStream {
-			receiver.execute(exchange.requestBody, it)
-		}
+	}
+	
+	/**
+	 * Does the actual command execution.
+	 * 
+	 * Subclasses can override this method to wrap the execution.
+	 */
+	protected void doExecute(InputStream input, OutputStream output) {
+		receiver.execute(input, output)
 	}
 }
