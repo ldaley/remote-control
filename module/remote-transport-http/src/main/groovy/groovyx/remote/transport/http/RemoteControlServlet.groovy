30d35a90773799c4cbb130ca396e9ac1f3380292
Luke Daley <ld@ldaley.com>
2013-01-16 14:06:27 +0000

    whitespace

--- module/remote-transport-http/src/main/groovy/groovyx/remote/transport/http/RemoteControlServlet.groovy
+++ module/remote-transport-http/src/main/groovy/groovyx/remote/transport/http/RemoteControlServlet.groovy
@@ -15,71 +15,73 @@
  */
 package groovyx.remote.transport.http
 
-import javax.servlet.*
-import javax.servlet.http.*
-import groovyx.remote.*
-import groovyx.remote.server.*
+import groovyx.remote.server.Receiver
+
 import javax.servlet.ServletConfig
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 /**
  * A servlet implementation for receiving commands.
  */
 class RemoteControlServlet extends HttpServlet {
-	
-	Receiver receiver 
-	
-	void init(ServletConfig config) {
-		super.init(config)
-		receiver = createReceiver()
-	}
-	
-	void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (validateRequest(request, response)) {
-			configureSuccessfulResponse(response)
-			doExecute(request.inputStream, response.outputStream)
-		}
-	}
 
-	/**
-	 * Validate that this request is valid.
-	 * 
-	 * Subclasses should call this implementation before any custom validation.
-	 * 
-	 * If the request is invalid, this is the place to send back the appropriate headers/body.
-	 * 
-	 * @return true if the request is valid and should proceed, false if otherwise.
-	 */
-	protected boolean validateRequest(HttpServletRequest request, HttpServletResponse response) {
-		if (request.contentType != ContentType.COMMAND.value) {
-			response.sendError(415, "Only remote control commands can be sent")
-			return false
-		}
-		
-		true
-	}
-	
-	/**
-	 * Called when a request has been validated.
-	 * 
-	 * Subclasses should call this implementation to set the status code and return content type.
-	 */
-	protected void configureSuccessfulResponse(HttpServletResponse response) {
-		response.contentType = ContentType.RESULT.value
-	}
-	
-	/**
-	 * Hook for subclasses to wrap the actual execution.
-	 */
-	protected void doExecute(InputStream input, OutputStream output) {
-		receiver.execute(input, output)
-	}
-	
-	/**
-	 * Hook for subclasses to provide a custom receiver. Will be called during init().
-	 * 
-	 * This implement returns a receiver created via the default constructor.
-	 */
-	protected Receiver createReceiver() {
-		new Receiver()
-	}
+    Receiver receiver
+
+    void init(ServletConfig config) {
+        super.init(config)
+        receiver = createReceiver()
+    }
+
+    void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (validateRequest(request, response)) {
+            configureSuccessfulResponse(response)
+            doExecute(request.inputStream, response.outputStream)
+        }
+    }
+
+    /**
+     * Validate that this request is valid.
+     *
+     * Subclasses should call this implementation before any custom validation.
+     *
+     * If the request is invalid, this is the place to send back the appropriate headers/body.
+     *
+     * @return true if the request is valid and should proceed, false if otherwise.
+     */
+    protected boolean validateRequest(HttpServletRequest request, HttpServletResponse response) {
+        if (request.contentType != ContentType.COMMAND.value) {
+            response.sendError(415, "Only remote control commands can be sent")
+            return false
+        }
+
+        true
+    }
+
+    /**
+     * Called when a request has been validated.
+     *
+     * Subclasses should call this implementation to set the status code and return content type.
+     */
+    protected void configureSuccessfulResponse(HttpServletResponse response) {
+        response.contentType = ContentType.RESULT.value
+    }
+
+    /**
+     * Hook for subclasses to wrap the actual execution.
+     */
+    protected void doExecute(InputStream input, OutputStream output) {
+        receiver.execute(input, output)
+    }
+
+    /**
+     * Hook for subclasses to provide a custom receiver. Will be called during init().
+     *
+     * This implement returns a receiver created via the default constructor.
+     */
+    protected Receiver createReceiver() {
+        new Receiver()
+    }
 }
